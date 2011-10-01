b7436ecb4c2df45c5c7d43af3d73e3e8c85f3c59
Moritz Lipp <mlq@pwmt.org>
2011-10-01 23:30:08 +0200

    Implement pdf_document_meta_get

--- pdf.h
+++ pdf.h
@@ -22,6 +22,7 @@ bool pdf_document_free(zathura_document_t* document);
 girara_tree_node_t* pdf_document_index_generate(zathura_document_t* document);
 bool pdf_document_save_as(zathura_document_t* document, const char* path);
 zathura_list_t* pdf_document_attachments_get(zathura_document_t* document);
+char* pdf_document_meta_get(zathura_document_t* document, zathura_document_meta_t meta);
 zathura_page_t* pdf_page_get(zathura_document_t* document, unsigned int page);
 zathura_list_t* pdf_page_search_text(zathura_page_t* page, const char* text);
 zathura_list_t* pdf_page_links_get(zathura_page_t* page);
