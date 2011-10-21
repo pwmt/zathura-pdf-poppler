ac4171f7f8ae29b2016915d9f33453ec2d3b336e
Sebastian Ramacher <s.ramacher@gmx.at>
2011-10-22 00:12:46 +0200

    remove zathura_list_t

--- pdf.h
+++ pdf.h
@@ -21,12 +21,12 @@ bool pdf_document_open(zathura_document_t* document);
 bool pdf_document_free(zathura_document_t* document);
 girara_tree_node_t* pdf_document_index_generate(zathura_document_t* document);
 bool pdf_document_save_as(zathura_document_t* document, const char* path);
-zathura_list_t* pdf_document_attachments_get(zathura_document_t* document);
+girara_list_t* pdf_document_attachments_get(zathura_document_t* document);
 char* pdf_document_meta_get(zathura_document_t* document, zathura_document_meta_t meta);
 zathura_page_t* pdf_page_get(zathura_document_t* document, unsigned int page);
-zathura_list_t* pdf_page_search_text(zathura_page_t* page, const char* text);
-zathura_list_t* pdf_page_links_get(zathura_page_t* page);
-zathura_list_t* pdf_page_form_fields_get(zathura_page_t* page);
+girara_list_t* pdf_page_search_text(zathura_page_t* page, const char* text);
+girara_list_t* pdf_page_links_get(zathura_page_t* page);
+girara_list_t* pdf_page_form_fields_get(zathura_page_t* page);
 #if !POPPLER_CHECK_VERSION(0,18,0)
 zathura_image_buffer_t* pdf_page_render(zathura_page_t* page);
 #endif
