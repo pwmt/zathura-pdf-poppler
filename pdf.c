9f92281fa529c7e301da0fc31917a116e0077ebe
Sebastian Ramacher <s.ramacher@gmx.at>
2011-10-23 17:05:26 +0200

    update includes due to girara restructuring

--- pdf.c
+++ pdf.c
@@ -1,6 +1,7 @@
 /* See LICENSE file for license and copyright information */
 
 #include <stdlib.h>
+#include <girara/datastructures.h>
 #include <string.h>
 #include <time.h>
 #include <poppler/glib/poppler.h>
