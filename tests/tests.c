342e569063c559764cbba6e658aa6b07ab4aebee
Moritz Lipp <mlq@pwmt.org>
2014-11-19 02:09:58 +0100

    Implement and test pdf_page_render_cairo

--- tests/tests.c
+++ tests/tests.c
@@ -11,6 +11,7 @@ Suite* suite_metadata(void);
 Suite* suite_search(void);
 Suite* suite_select(void);
 Suite* suite_links(void);
+Suite* suite_render(void);
 
 int main(void)
 {
@@ -31,6 +32,7 @@ int main(void)
   srunner_add_suite(suite_runner, suite_search());
   srunner_add_suite(suite_runner, suite_select());
   srunner_add_suite(suite_runner, suite_links());
+  srunner_add_suite(suite_runner, suite_render());
 
   int number_failed = 0;
   srunner_run_all(suite_runner, CK_ENV);
