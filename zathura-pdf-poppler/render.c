70c6f762e618f42581a73be72282a0a4a5fbc39c
Emanuel Tobias Pichler <emanuel.pichler@student.tugraz.at>
2023-09-29 17:34:09 +0200

    Remove unnecessary whitespace

--- zathura-pdf-poppler/render.c
+++ zathura-pdf-poppler/render.c
@@ -5,7 +5,7 @@
 zathura_error_t
 pdf_page_render_cairo(zathura_page_t* page, void* data, cairo_t*
     cairo, bool printing)
-{  
+{
   if (page == NULL || data == NULL || cairo == NULL) {
     return ZATHURA_ERROR_INVALID_ARGUMENTS;
   }
